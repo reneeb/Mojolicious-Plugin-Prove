1e3d666aa0bd8b4d1571e1fd78197c79b03d680b
reneeb <github@renee-baecker.de>
2015-01-25 00:49:49 +0100

    bring Changes in sync

--- lib/Mojolicious/Plugin/Prove.pm
+++ lib/Mojolicious/Plugin/Prove.pm
@@ -7,7 +7,7 @@ use Mojo::Base 'Mojolicious::Plugin::Prove::Base';
 use File::Basename;
 use File::Spec;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub register {
     my ($self, $app, $conf) = @_;
