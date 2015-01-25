e8d174f753ba06f522a29179cd08800651db9edf
reneeb <github@renee-baecker.de>
2015-01-25 12:18:27 +0100

    prepare release for 0.06

--- lib/Mojolicious/Plugin/Prove.pm
+++ lib/Mojolicious/Plugin/Prove.pm
@@ -7,7 +7,7 @@ use Mojo::Base 'Mojolicious::Plugin::Prove::Base';
 use File::Basename;
 use File::Spec;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub register {
     my ($self, $app, $conf) = @_;
